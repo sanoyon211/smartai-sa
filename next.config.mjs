aa923b6cdd0f873b1f3853ba52d8e6daa227fabb
sanoyon211 <sanoyon522@gmail.com>
2026-05-07 13:29:49 +0600

    fix next.config.mjs turbopack config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ["@prisma/client"],
-
-  turbopack: {
-    resolveAlias: {
-      "@prisma/client": { edge: false },
-      "better-auth/adapters/prisma": { edge: false },
-    },
-  },
+  turbopack: {},
 };
 
 export default nextConfig;
