82b32d007bd8d567a3a69fb21b41d631b461580f
sanoyon211 <sanoyon522@gmail.com>
2026-05-07 12:42:03 +0600

    solving deployment problems in this project

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["@prisma/client"],
+  },
+  webpack: (config, { isServer, nextRuntime }) => {
+    if (isServer && nextRuntime === "edge") {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        "@prisma/client": false,
+        "better-auth/adapters/prisma": false,
+      };
+    }
+    return config;
+  },
+
+};
+
 
 export default nextConfig;
